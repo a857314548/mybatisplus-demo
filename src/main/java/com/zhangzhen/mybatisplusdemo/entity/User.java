8f5f8140d523c54e5bc32bbb19df3ac46d655ef9
zwx746910 <857314548@qq.com>
2021-02-15 19:45:47 +0800

    2021.02.15代码提交

--- src/main/java/com/zhangzhen/mybatisplusdemo/entity/User.java
+++ src/main/java/com/zhangzhen/mybatisplusdemo/entity/User.java
@@ -1,11 +1,26 @@
 package com.zhangzhen.mybatisplusdemo.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class User {
+    //@TableId(type = IdType.AUTO)自增
+    //@TableId(type = IdType.ID_WORKER) 用于long
+    //@TableId(type = IdType.ID_WORKER_STR) 用于字符串
     private Long id;
     private String name;
     private Integer age;
     private String email;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
 }
--- src/test/java/com/zhangzhen/mybatisplusdemo/MybatisplusDemoApplicationTests.java
+++ src/test/java/com/zhangzhen/mybatisplusdemo/MybatisplusDemoApplicationTests.java
@@ -20,4 +20,23 @@ public class MybatisplusDemoApplicationTests {
         System.out.println(users);
     }
 
+    @Test
+    public void insertOne() {
+        User user = new User();
+        user.setName("测试张小凡");
+        user.setAge(20);
+        user.setEmail("2114059980@qq.com");
+        int insert = userMapper.insert(user);
+        System.out.println("insert:" + insert);
+    }
+
+    @Test
+    public void update() {
+        User user = new User();
+        user.setId(1361279559005110274L);
+        user.setAge(25);
+        int num = userMapper.updateById(user);
+        System.out.println("num:" + num);
+    }
+
 }
