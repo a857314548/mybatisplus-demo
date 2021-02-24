1874d174fab069252f6b46f0e95b50a9cc6e807b
zwx746910 <857314548@qq.com>
2021-02-24 22:31:07 +0800

    2021.02.24代码提交,学习muybatisplus各种复杂查询

--- src/main/java/com/zhangzhen/mybatisplusdemo/entity/User.java
+++ src/main/java/com/zhangzhen/mybatisplusdemo/entity/User.java
@@ -24,4 +24,8 @@ public class User {
     @Version
     @TableField(fill = FieldFill.INSERT)
     private Integer version;
+
+    @TableLogic
+    //@TableField(fill = FieldFill.INSERT)
+    private Integer deleted;
 }
--- src/test/java/com/zhangzhen/mybatisplusdemo/MybatisplusDemoApplicationTests.java
+++ src/test/java/com/zhangzhen/mybatisplusdemo/MybatisplusDemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.zhangzhen.mybatisplusdemo;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zhangzhen.mybatisplusdemo.entity.User;
@@ -29,13 +31,13 @@ public class MybatisplusDemoApplicationTests {
     }
 
     /**
-     * 查询单个
+     * 创建单个
      */
 
     @Test
     public void insertOne() {
         User user = new User();
-        user.setName("测试乐观锁");
+        user.setName("测试mybatisplus性能2");
         user.setAge(20);
         user.setEmail("2114059980@qq.com");
         int insert = userMapper.insert(user);
@@ -120,6 +122,124 @@ public class MybatisplusDemoApplicationTests {
         System.out.println(page.hasNext()); //是否有下一个数据
         System.out.println(page.hasPrevious()); //是否有上一个数据
 
+    }
+
+    /**
+     * 测试单条删除
+     */
+    @Test
+    public void testDelete() {
+        int i = userMapper.deleteById(1364559288407764994L);
+        System.out.println("删除条数:" + i);
+    }
+
+    /**
+     * 测试批量删除
+     */
+    @Test
+    public void testBatchDelete() {
+        int i = userMapper.deleteBatchIds(Arrays.asList(2L, 3L));
+        System.out.println("批量删除条数：" + i);
+    }
+
+    /**
+     * 测试逻辑删除
+     */
+    @Test
+    public void testLoginDelete() {
+    }
+
+    /**
+     * 测试条件删除
+     */
+    @Test
+    public void testConditionDelete() {
+        Map<String,Object> map = new HashMap<>();
+
+        map.put("name","Billie");
+        map.put("age","24");
+
+        int i = userMapper.deleteByMap(map);
+        System.out.println("条件删除条数:" + i);
+    }
+
+    /**
+     * 测试复杂查询
+     */
+    @Test
+    public void testComplexConditionQuery() {
+
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+
+        //ge gt le lt isNull isNotNull 大于等于 大于 小于等于 小于 为null 不为null
+        //queryWrapper.ge("age",24);
+        //queryWrapper.gt("age",24);
+        //queryWrapper.le("age",24);
+        //queryWrapper.lt("age",24);
+        //queryWrapper.isNull("create_time");
+        //queryWrapper.isNotNull("create_time");
+
+        //eq、ne 等于 不等于
+        //queryWrapper.eq("name","测试乐观锁");
+        //queryWrapper.ne("name","测试乐观锁");
+
+        //between、notBetween  某某范围之间,不在某某范围之间
+        //queryWrapper.between("age",21,24);
+        //queryWrapper.notBetween("age",21,24);
+
+        //allEq 根据多个字段查询
+        /*Map<String,Object> map = new HashMap<>();
+        map.put("id",4L);
+        map.put("name","Sandy");
+        map.put("age",21);
+        queryWrapper.allEq(map);*/
+
+        //like、notLike、likeLeft、likeRight 模糊匹配 模糊不匹配 左边模糊匹配 右边模糊匹配
+        //queryWrapper.like("name","张");
+        //queryWrapper.notLike("name","张");
+        //queryWrapper.likeLeft("name","y");
+        //queryWrapper.likeRight("name","S");
+
+        // in、notIn、inSql、notinSql、exists、notExists  在指定范围 不在指定范围 子查询 非子查询 存在 不存在
+        //queryWrapper.in("name","测试张小凡","测试逻辑删除");
+        //queryWrapper.notIn("name","测试张小凡","测试逻辑删除");
+        //SELECT id,name,age,email,create_time,update_time,version,deleted FROM user WHERE deleted=0
+        // AND age IN ( select age from user where age >= 21)
+        //queryWrapper.inSql("age","select age from user where age >= 21 ");
+        //queryWrapper.notInSql("age","select age from user where age >= 21 ");
+        //存在则根据前面条件全查出来 不存在则一条也不查
+        //queryWrapper.notExists("select age from user where age = 18");
+
+        //or、and 或者 和 and 这里用updateWapper
+        /*UpdateWrapper<User> userUpdateWrapper = new UpdateWrapper<>();
+        userUpdateWrapper.eq("age",20).or().likeLeft("name","锁");
+        List<User> users1 = userMapper.selectList(userUpdateWrapper);*/
+
+        //orderByDesc、orderByAsc  降序 升序
+        //queryWrapper.orderByDesc("id");
+        //queryWrapper.orderByAsc("id");
+
+        //last 直接拼接到 sql 的最后 limit 3 前三条 limit 1,3 从第4条开始往后查3条
+        //注意：只能调用一次,多次调用以最后一次为准 有sql注入的风险,请谨慎使用
+        //queryWrapper.last("limit 3");
+        //queryWrapper.last("limit 2,3");
+
+        //指定要查询的列
+        //queryWrapper.select("id","name","age");
+
+        //set、setSql 最终的sql会合并 user.setAge()，以及 userUpdateWrapper.set() 和 setSql() 中 的字段 这里使用updateWapper
+        /*User usera = new User();
+        usera.setAge(27);
+        UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.eq("name","Sandy").set("name","jhon").setSql("email = 'zwx746910@qq.com'");
+
+        userMapper.update(usera,updateWrapper);*/
+
+        List<User> users = userMapper.selectList(queryWrapper);
+        //System.out.println(users);
+        users.forEach(user -> {
+            System.out.println(user + "\t");
+        });
     }
 
 }
